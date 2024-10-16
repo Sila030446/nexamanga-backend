da3cfde302d5d95ad35620eba664e6640ae88895
SilaPrtnw. <99876548+Sila030446@users.noreply.github.com>
2024-10-16 17:56:26 +0700

    fix git error

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,10 +9,14 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'https://manhwa-heaven-project.vercel.app',
-    ], // Replace with your frontend URL
+      'http://nexamanga.online',
+      'https://nexamanga.online',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(3000);
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(8888);
 }
 bootstrap();
